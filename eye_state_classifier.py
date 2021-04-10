a9ea6839b322e47f32a0d9a4842030f2041beca7
Phoom Punpeng <phoomp14@gmail.com>
2021-04-10 20:54:23 +0700

    Added eyestate model

--- eye_state_classifier.py
+++ eye_state_classifier.py
@@ -1,14 +1,43 @@
 import numpy as np
 import tensorflow as tf
+import pathlib
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-print(tf.__version__)
+from matplotlib import pyplot as plt
 
-img_height = 100
-img_width = 100
+print(tf.__version__)
+print('GPUs Available: ', len(tf.config.list_physical_devices('GPU')))
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+data_dir = pathlib.Path('./eyestate')
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 model = Sequential([
     layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
@@ -26,3 +55,36 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.summary()
+
+
+epochs = 10
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+
+model.save('saved_model/eyestate')
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
